8c2e73f0e0e2b0b94b1f2c1f1bddf6db728b42d3
RVA <ravi.vasudevan@hyetsolar.com>
2024-01-05 23:40:53 +0100

    initiated global.R

---
+++ global.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(plotly)
+library(shiny)
+library(shinyWidgets)
+library(thematic)
+library(DT)
+library(bslib)
+library(bsicons)
+theme_set(theme_minimal())
+
+thematic_shiny(font = "auto")
+
+enableBookmarking("url")
