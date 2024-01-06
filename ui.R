7795dd5a259948966039d199735dd9d763a23492
RVA <ravi.vasudevan@hyetsolar.com>
2024-01-06 08:14:11 +0100

    Added logos and links to navbar

--- ui.R
+++ ui.R
@@ -1,33 +1,38 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-)
+function(reqest){
+  page_navbar(theme = 
+                bs_theme(
+                  bootswatch = "lux", version = 5,
+                  primary = "#fc0",
+                  secondary = "#545454",
+                  "navbar-bg" = "#000",
+                  "navbar-text-color" = "#fc0 !important",
+                  "nav-link-color" = "#FC0 !important",
+                  "nav-link-hover-color" = "#FC0 !important"
+                  
+                ),
+              title=img(
+                src = "European_Ultimate_Federation_EUF_Logo.png",
+                height = 50
+              ),
+              sidebar = sidebar(
+                selectInput(inputId = "year", "Year:",
+                            choices = c(2024)),
+                selectInput(inputId = "division", "Division:",
+                            choices = c("Mixed", "Open", "Women"), selected = "Mixed")
+              ),
+              nav_item("EUCS Explorer"),
+              nav(title = "Season"),
+              nav(title = "Team"),
+              nav(title = "Event"),
+              nav_spacer(),
+              nav_item(a(bs_icon(name = "instagram"),
+                         href="http://www.instagram.com/ultimatefederation_eu")),
+              nav_item(a(bs_icon(name = "facebook"),
+                         href="https://www.facebook.com/ultimate.eu/")),
+              nav_item(a(bs_icon(name = "envelope"),
+                         href="mailto:competition@ultimatefederation.eu"))
+              
+              
+              
+  )
+}
