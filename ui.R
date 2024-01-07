22379613502cf341a64f07fae878d682bb17f7cb
vasurav <ravi.a.vasudevan@gmail.com>
2024-01-07 11:34:07 +0100

    UI Tweaks to match EUF Style

--- ui.R
+++ ui.R
@@ -7,23 +7,27 @@ function(reqest){
                   "navbar-bg" = "#000",
                   "navbar-text-color" = "#fc0 !important",
                   "nav-link-color" = "#FC0 !important",
-                  "nav-link-hover-color" = "#FC0 !important"
+                  "nav-link-font-size" = "25px",
+                  "nav-link-font-weight" = "normal",
+                  "nav-text-color" = "#fc0 !important",
+                  "nav-link-hover-color" = "#FC0 !important",
+                  base_font = font_google("Bebas Neue")
                   
                 ),
               title=img(
                 src = "European_Ultimate_Federation_EUF_Logo.png",
-                height = 50
+                height = 80
               ),
+              window_title = "EUCS Explorer",
               sidebar = sidebar(
                 selectInput(inputId = "year", "Year:",
                             choices = c(2024)),
                 selectInput(inputId = "division", "Division:",
                             choices = c("Mixed", "Open", "Women"), selected = "Mixed")
               ),
-              nav_item("EUCS Explorer"),
-              nav(title = "Season"),
-              nav(title = "Team"),
-              nav(title = "Event"),
+              nav_panel(title = "Season"),
+              nav_panel(title = "Team"),
+              nav_panel(title = "Event"),
               nav_spacer(),
               nav_item(a(bs_icon(name = "instagram"),
                          href="http://www.instagram.com/ultimatefederation_eu")),
