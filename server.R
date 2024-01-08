51587770e0b3b5a1473cb43fcb5c124ae84f296a
RVA <ravi.vasudevan@hyetsolar.com>
2024-01-08 14:52:58 +0100

    fixed "Counted" bug. Was looking at Is_Ignored_Windmill instead of Is_Ignored_USAU

--- server.R
+++ server.R
@@ -67,7 +67,7 @@ function(input, output, session) {
                                   Opponent_Rating + Game_Rank_Diff_USAU, 
                                   Opponent_Rating - Game_Rank_Diff_USAU) %>% 
                round(2)) %>% 
-      mutate(Counted = ifelse(Is_Ignored_Windmill, "No", "Yes"))
+      mutate(Counted = ifelse(Is_Ignored_USAU, "No", "Yes"))
   })
   
   output$team_selector <- renderUI({
