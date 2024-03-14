79cff68305239e5d52878363edc7079132858f58
RVA <ravi.vasudevan@hyetsolar.com>
2024-03-14 15:11:05 +0100

    changed padding to leave room for sidebar on mobile.

--- ui.R
+++ ui.R
@@ -19,7 +19,7 @@ function(reqest){
                 @media (max-width: 575.98px) {
                   .main {
                     padding: 5px !important;
-                    padding-left: 30px !important;
+                    padding-left: 40px !important;
                   }
                 }
               ")
