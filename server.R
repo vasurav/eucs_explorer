dc539b1362a7e47362dd521402ecbcb0f1b71a80
vasurav <ravi.a.vasudevan@gmail.com>
2024-08-17 21:27:18 +0200

    updated some evolution plots.

--- server.R
+++ server.R
@@ -176,6 +176,12 @@ function(input, output, session) {
       filter(Ranking %in% 1:number_of_teams) %>% pull(Team) %>% unique
     
     y_var <- sym(input$season_evolution_type)
+    label_var <- 
+      case_match(
+        input$season_evolution_type,
+        "Rating" ~ "Ranking",
+        "Ranking" ~ "Rating") %>% 
+      sym
     
     season_evo_data <- season_evo_data %>%
       filter(Team %in% top_teams)
@@ -184,14 +190,14 @@ function(input, output, session) {
     `EUCF Cutoff (Including Unassigned Wildcards)` = 16 + 0.5
     
     plt <- season_evo_data %>% 
-        ggplot(aes(x=Ranking_Calculation_Date, y=!!y_var, color=Team)) +
+        ggplot(aes(x=Ranking_Calculation_Date, y=!!y_var, color=Team, label=!!label_var)) +
         geom_line() + geom_point() 
     
     if(input$season_evolution_type == "Ranking")
       plt <- plt +
       geom_hline(aes(yintercept = `EUCF Cutoff (Including Unassigned Wildcards)`, 
                      linetype="EUCF Cutoff (Including Unassigned Wildcards)"),
-                 color=color_eucf_likely_dark, 
+                 color=color_eucf_likely_dark
       ) +
       geom_hline(aes(yintercept = `EUCF Cutoff`, 
                      linetype="EUCF Cutoff"),
@@ -200,7 +206,7 @@ function(input, output, session) {
       scale_y_reverse(limits = c(NA,1), breaks=c(1,seq(5,max(season_evo_data$Ranking),5)))
     
     plt %>% 
-      ggplotly(tooltip = c("color","shape", "y", "x", "yintercept")) %>% hide_legend()
+      ggplotly(tooltip = c("color","shape", "y", "x", "label", "yintercept")) %>% hide_legend()
     
   })
   
@@ -350,10 +356,11 @@ function(input, output, session) {
   })
   
   output$team_evolution_plot <- renderPlotly({
-    summary_data %>% 
+    (summary_data %>% 
+       rename(Rating = Rating_USAU) %>% 
       filter(Season == input$season, Division == input$division) %>% 
       filter(Team == input$team) %>% 
-      ggplot(aes(x=Ranking_Calculation_Date, y=Ranking, color=Team)) +
+      ggplot(aes(x=Ranking_Calculation_Date, y=Ranking, color=Team, label=Rating)) +
       geom_line() + geom_point() +
       scale_y_reverse(limits = c(NA,1)) +
       geom_hline(yintercept = 16.5, 
@@ -361,7 +368,8 @@ function(input, output, session) {
                  linetype="dashed") +
       geom_hline(yintercept = find_eucf_cutoff() + 0.5, 
                  color=color_eucf_guaranteed_dark, 
-                 linetype="dashed")
+                 linetype="dashed")) %>% 
+      ggplotly %>% hide_legend
   })
   
   eucf_cutoff_rating_guaranteed <- reactive({
